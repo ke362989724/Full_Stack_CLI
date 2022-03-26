de62908897ed2e6663d5c8016469b1a9b95b3cad
unknown <ke362989724@gmail.com>
2022-03-27 06:12:56 +0800

    Changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,7 +13,10 @@ function Login() {
   const login = () => {
     const data = { username: username, password: password };
     axios
-      .post("https://full-stack-tutorial-ke36298972.herokuapp.com/login", data)
+      .post(
+        "https://full-stack-tutorial-ke36298972.herokuapp.com/auth/login",
+        data
+      )
       .then((response) => {
         if (response.data.error) {
           alert(response.data.error);
